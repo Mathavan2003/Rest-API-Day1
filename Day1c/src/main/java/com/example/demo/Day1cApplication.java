76786750987de3d12f118bd38f505a142e2f149e
Mathavan2003 <108514155+Mathavan2003@users.noreply.github.com>
2023-08-18 15:09:33 +0530

    Add files via upload

---
+++ Day1c/src/main/java/com/example/demo/Day1cApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1cApplication.class, args);
+	}
+
+}
